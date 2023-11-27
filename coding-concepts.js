8b4b861345208a3e4516217084c41e1648ae4abf
jwat5623 <jamar34watson56@gmail.com>
2023-11-27 03:24:18 -0800

    finished assessment

--- coding-concepts.js
+++ coding-concepts.js
@@ -8,48 +8,48 @@
 
 // --------------------INSTRUCTOR EXAMPLE: What will this log?
 const colors = ["tangerine", "magenta", "lilac", "daffodil"]
-// console.log(colors.push("indigo"))
+console.log(colors.push("indigo"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it will add indigo string to end of array
+// b) Verify and explain: partially correct, indigo was added but what was logged in the console was the index
 
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2023"
-// console.log(cohort.length)
+console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: the length of the string
+// b) Verify and explain: correct, the length of the string including the space
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: the value of the 4th index which is "o"
+// b) Verify and explain: correct "o" was the outcome
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+ console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: the second string in the array will print
+// b) Verify and explain: correct, "Ruby" printed
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+//console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: every string in the array will be made Uppercase
+// b) Verify and explain: incorrect, "It's not a function"
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it will kick an error
+// b) Verify and explain: incorrect, number was the output, i thought it would be an error because its asking for the datatypes but they are all strings
